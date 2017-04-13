ed3dee116ccc49f6987adb15f15b3e0c7634663c
Paul Lewallen <paul.lewallen@gmail.com>
2017-04-13 12:04:46 -0700

    init

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            // Bug for demo purposes
+
+            return a + b;
+        }
+    }
+}
